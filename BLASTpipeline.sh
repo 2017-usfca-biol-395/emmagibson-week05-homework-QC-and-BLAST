f80f9d1d8c05f8e0a7c5a9111e6d87b0d28dd18c
Emma Gibson <egibson@dons.usfca.edu>
2017-09-25 18:43:40 +0000

    add sequence trimming to code

--- BLASTpipeline.sh
+++ BLASTpipeline.sh
@@ -10,11 +10,26 @@ if [ -z ${BLASTDB} ]; then source /home/.bashrc; export PATH; export BLASTDB; fi
 
 # download list of files in the run table to raw_data directory
 # the pipe and 'tail -n +2' excludes the first line
+echo "downloading raw data..."
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
     fastq-dump -v $SRA_number -O data/raw_data
 done
+echo "downloaded raw data"
 
 # create QC reports for each run
 fastqc data/raw_data/*.fastq --outdir=output/fastqc
+
+# trim sequences based on quality scores
+# $@ should be something like 'data/raw_data/*.fastq'
+# first, make the output directory
+echo "making trimmed sequence directory..."
+mkdir data/trimmed_seqs
+echo "made trimmed sequence directory"
+echo "cleaning .fastq files..."
+for file in $@
+do 
+	TrimmomaticSE -threads 2 -phred33 $file data/trimmed_seqs/$(basename -s .fastq $file).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+done
+echo "cleaned .fastq files"
 
