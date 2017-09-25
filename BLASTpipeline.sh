07ea1e584c8fd6b00f157994c7213c449d6dce07
Emma Gibson <egibson@dons.usfca.edu>
2017-09-25 17:45:46 +0000

    add script toset up BLAST database and download data files

---
+++ BLASTpipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Emma Gibson egibson@dons.usfca.edu
+# September 27, 2017
+# Practice script to run a BLAST pipeline
+
+# check for BLASTDB variable, and sets appropriate variables if there isn't one
+if [ -z ${BLASTDB} ]; then source /home/.bashrc; export PATH; export BLASTDB; fi
+
+
+# download list of files in the run table to raw_data directory
+# the pipe and 'tail -n +2' excludes the first line
+for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
+do
+	fastq-dump -v $SRA_number -0 data/raw_data
+done
